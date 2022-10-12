6b6ba3269a9d8b2f4d5c51c7cdadcaf3c137f777
emanuel heredia <ema_359@hotmail.com>
2022-10-12 14:17:10 -0300

    finalizando proyecto creando state alertas formulario

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import { crearNuevoProductoAction } from "../action/productosAction";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { mostrarAlerta, ocultarAlerta } from "../action/alertaAction";
 
-const NuevoProducto = ({ history }) => {
+const NuevoProducto = () => {
 	const navigate = useNavigate();
 	const [nombre, setNombre] = useState("");
 	const [precio, setPrecio] = useState("");
@@ -13,12 +14,20 @@ const NuevoProducto = ({ history }) => {
 	};
 	const cargando = useSelector((state) => state.productos.loading);
 	const error = useSelector((state) => state.productos.error);
+	const alerta = useSelector((state) => state.alerta.alerta);
 	const submitNuevoProducto = (e) => {
 		e.preventDefault();
 		if (nombre.trim() === "" || precio <= 0) {
+			const alerta = {
+				msg: "Ambos capos son obligatorios",
+				classes: "alert alert-danger text-center text-uppercase p-3",
+			};
+			console.log("error");
+			dispatch(mostrarAlerta(alerta));
 			return;
 		}
 		agregarProducto({ nombre, precio });
+		dispatch(ocultarAlerta());
 		//Redireccionar
 		navigate("/");
 	};
@@ -30,6 +39,9 @@ const NuevoProducto = ({ history }) => {
 						<h2 className="text-center mb-4 font-weight-bold">
 							Agregar Nuevo Producto
 						</h2>
+						{alerta && (
+							<p className={alerta.classes}>{alerta.msg}</p>
+						)}
 						<form onSubmit={submitNuevoProducto}>
 							<div className="form-group">
 								<label>Nombre del Producto</label>
--- src/reducers/alertaReducer.js
+++ src/reducers/alertaReducer.js
@@ -4,6 +4,16 @@ const inicialState = { alerta: null };
 
 export default function (state = inicialState, action) {
 	switch (action.type) {
+		case MOSTRAR_ALERTA:
+			return {
+				...state,
+				alerta: action.payload,
+			};
+		case OCULTAR_ALERTA:
+			return {
+				...state,
+				alerta: null,
+			};
 		default:
 			return state;
 	}
